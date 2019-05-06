d3abd0759bc3e3e2deb59c4d7a370316b73f1b06
dsl14122 <1596720698@qq.com>
2019-05-06 10:58:01 +0800

    login 组件添加

--- src/router.js
+++ src/router.js
@@ -3,10 +3,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+import login from "./components/login.vue"
+
 //写组件
+ 
 
 // 写规则
-const routes=[]
+const routes=[{
+    path:'/login',
+    component:login
+}]
 // 实例化路由对象
  const router = new VueRouter({
      routes
