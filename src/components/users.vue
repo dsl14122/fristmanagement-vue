e6e35d25b0c8960f44c1021603326392e49a9af7
dsl14122 <1596720698@qq.com>
2019-05-08 21:01:12 +0800

    users增删操作

--- src/components/users.vue
+++ src/components/users.vue
@@ -14,27 +14,43 @@
         </el-input>
       </el-col>
       <el-col :span="12">
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" @click="addVisible=true" plain>添加用户</el-button>
       </el-col>
     </el-row>
     <!-- 表格 -->
     <template>
       <el-table :data="tableData" border style="width: 100%">
         <el-table-column type="index" width="50"></el-table-column>
-        <el-table-column prop="role_name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="username" label="姓名" width="180"></el-table-column>
         <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
         <el-table-column prop="mobile" label="电话" width="180"></el-table-column>
         <el-table-column prop="mg_state" label="用户状态" width="180">
           <template slot-scope="scope">
-          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="stateChange(scope.row)"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            ></el-switch>
           </template>
         </el-table-column>
         <el-table-column prop="address" label="操作">
-          
           <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)" size='mini' plain></el-button>
-            <el-button type="success" icon="el-icon-check" size='mini' plain></el-button>
-            <el-button type="danger" icon="el-icon-delete" @click="handleDelete(scope.$index, scope.row)" size='mini' plain></el-button>
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              @click="handleEdit(scope.$index, scope.row)"
+              size="mini"
+              plain
+            ></el-button>
+            <el-button type="success" icon="el-icon-check" size="mini" plain></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              @click="handleDelete(scope.$index, scope.row)"
+              size="mini"
+              plain
+            ></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -47,14 +63,37 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="50"
     ></el-pagination>
+    <!-- 增加用户弹框 -->
+    <el-dialog title="添加用户" :visible.sync="addVisible">
+      <el-form :model="addForm" :rules="addRules" ref="addForm">
+        <el-form-item label="用户名" label-width="120px" prop="username">
+          <el-input v-model="addForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="120px" prop="password">
+          <el-input v-model="addForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="120px">
+          <el-input v-model="addForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="120px">
+          <el-input v-model="addForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('addForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: "users",
+
   data() {
     return {
+      addVisible: false,
       tableData: [
         {
           date: "1",
@@ -81,24 +120,100 @@ export default {
         query: "",
         pagenum: 1,
         pagesize: 10
+      },
+      addRules: {
+        username: [
+          { required: true, message: "用户名不为空", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不为空", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        ]
+      },
+      addForm: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
       }
     };
   },
   methods: {
-    handleEdit(index,row){
-    console.log(row);
-
+    handleEdit(index, row) {
+      // console.log(row);
     },
-    handleDelete(index,row){
-     console.log(row);
+    //删除用户
+    handleDelete(index, row) {
+      // console.log(row);
+      //弹框确认
+        this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+          confirmButtonText: '干了它',
+          cancelButtonText: '算了吧',
+          type: 'warning'
+        }).then(() => {
+          // 调用接口
+          this.$request.deleteUsers(row.id).then(res=>{
+              // console.log(res);
+              // 重新获取数据
+            if (res.data.meta.status == 200) {
+               this.getusers();
+            }
+            })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '算了算了，不干它了'
+          });          
+        });
+
+
+
+      
+    },
+    //更改状态
+    stateChange(row) {
+      this.$request
+        .updateStatus({ id: row.id, type: row.mg_state })
+        .then(res => {
+          //  console.log(res);
+        });
+    },
+    //增添用户
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 数据正确
+          this.$request.addUsers(this.addForm).then(res => {
+            console.log(res);
+            // 关闭弹框
+            this.addVisible = false;
+            // 重新获取数据
+            this.getusers();
+            // 重置表单
+            
+          });
+        } else {
+          // 数据有误
+          this.$message.error("数据格式不对！");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    // 数据获取
+    getusers() {
+      this.$request.getusers(this.userData).then(res => {
+        // console.log(res);
+        this.tableData = res.data.data.users;
+      });
     }
   },
   // 调用接口
   created() {
-    this.$request.getusers(this.userData).then(res => {
-      // console.log(res);
-      this.tableData = res.data.data.users;
-    });
+    this.getusers();
   }
 };
 </script>
--- src/libs/http.js
+++ src/libs/http.js
@@ -1,8 +1,32 @@
 
 // 导入 axios
 import axios from 'axios'
-
+import Vue from 'vue'
 axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+
+// 添加一个请求拦截器
+axios.interceptors.request.use(function (config) {
+    //  console.log(config);
+   config.headers.Authorization=window.sessionStorage.getItem('token');
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// 添加一个拦截器的反应
+axios.interceptors.response.use(function (response) {
+    // console.log(response);
+    if(response.data.meta.status==200){
+        Vue.prototype.$message.success(response.data.meta.msg)
+    }
+    
+    return response;
+  }, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+  });
+
 //抽取网络请求的对象
 const request={
     login(params){
@@ -11,11 +35,20 @@ const request={
     //获取用户
     getusers(params){
         return axios.get('users',{
-            params,
-            headers:{
-                Authorization:window.sessionStorage.getItem('token')
-            }
+            params
         })
+    },
+    //修改用户状态
+    updateStatus(params){
+        return axios.put(`users/${params.id}/state/${params.type}`)
+    },
+    // 增加用户
+    addUsers(params){
+        return axios.post('users',params)
+    },
+    //删除用户
+    deleteUsers(id){
+        return axios.delete(`users/${id}`)
     }
 }
 
